f10fe46a2b2e1bab925d9171eb0df9ae611b7bb1
DaveParillo <DaveParillo@users.noreply.github.com>
2017-01-27 17:08:47 -0800

    added tasha to solutions distribution

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 tar cyf solutions.zip -C ./src --exclude .DS_Store solutions
-gpg --output solutions.gpg --encrypt --armor --recipient dparillo@sdccd.edu solutions.zip
+gpg --output solutions.gpg --encrypt --armor --recipient dparillo@sdccd.edu --recipient tashacv@gmail.com solutions.zip
 rm solutions.zip
 
 
