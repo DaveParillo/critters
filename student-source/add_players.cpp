4f978d0ad1971b7b88df927cf306b23a80df2fe6
DaveParillo <DaveParillo@users.noreply.github.com>
2019-10-04 21:29:25 -0700

    remove penguin

--- student-source/add_players.cpp
+++ student-source/add_players.cpp
@@ -1,7 +1,6 @@
 
 // add your competitor(s) include files here
 #include "Olympian.h"
-#include "Penguin.h"
 
 #include <Critter.h>
 #include <memory>
@@ -14,7 +13,6 @@ players add_players () {
 
   // push back your Critter onto the players vector
   p.push_back(std::make_shared<Olympian>());
-  p.push_back(std::make_shared<Penguin>());
 
   return p;
 }
